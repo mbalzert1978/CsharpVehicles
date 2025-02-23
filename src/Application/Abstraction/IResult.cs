8892b85aeb2cecd0877884692fc6eb2ccfd55049
Markus Iorio <mbalzert@gmail.com>
2025-02-23 18:49:50 +0100

    Rename parameter in IResult<T, TError>.UnwrapOr to avoid conflict with reserved keyword

--- src/Application/Abstraction/IResult.cs
+++ src/Application/Abstraction/IResult.cs
@@ -8,7 +8,7 @@ public interface IResult<T, TError>
     [SuppressMessage(
         "Naming",
         @"CA1716: 
-         In virtual/interface member IOption<T>.UnwrapOr(T), rename parameter default
+         In virtual/interface member IResult<T, TError>.UnwrapOr(T), rename parameter default
          so that it no longer conflicts with the reserved language keyword 'default'.
          Using a reserved keyword as the name of a parameter on a virtual/interface 
          member makes it harder for consumers in other languages
